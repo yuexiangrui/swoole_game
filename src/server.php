f5a041be319e7da1b9944c86d724d9821c086208
yuexiangrui <yuexiangrui@chelun.com>
2018-11-14 10:10:28 +0800

    modified:   src/server.php 	modified:   src/server.pid

--- src/server.php
+++ src/server.php
@@ -1,22 +1,63 @@
 #!/usr/bin/env php
 <?php
+namespace  swoole;
+
 class swoole_new_server {
 
     public  $iport = 9505;
-    public  $ip = '127.0.0.1';
+    public  $ip = '0.0.0.0';
     public  $serv = '';
     public  $pid =  '';
-
-    public function __construct()
+    public  $process_name = 'test';
+    public  $config = [];
+    public function __construct($ip='127.0.0.1',$iport=9505,$process_name = 'swoole_game')
     {
+        $this->ip = $ip;
+        $this->iport = $iport;
+        $this->process_name = $process_name;
+        $this->setConfig();
+    }
 
+    public function setConfig($config = []){
+        $swoole_config = array(
+            'reactor_num' => $config['reactor_num'] ?? 2,//通过此参数来调节poll线程的数量，以充分利用多核
+            'worker_num' => $config['worker_num'] ?? 8,// 开启进程
+//            'daemonize' => $config['daemonize'] ?? 0,//是否开启守护进程 0否 1 是
+            'max_request' => $config['max_request'] ?? 10000,//最大请求处理1万个 //此参数表示worker进程在处理完n次请求后结束运行。manager会重新创建一个worker进程。此选项用来防止worker进程内存溢出。
+            // "task_ipc_mode " => 3 ,  //使用消息队列通信，并设置为争抢模式
+            'backlog' => $config['backlog'] ?? 128,//Listen队列长度
+            'pid_file'=> $config['pid_file'] ?? __DIR__.'/server.pid',//进程唯一标识ID
+            'log_level' => $config['log_level'] ?? 1,
+//            'tcp_defer_accept' => $config['reactor_num'] ?? 5,//此参数设定一个秒数，当客户端连接连接到服务器时，在约定秒数内并不会触发accept，直到有数据发送，或者超时时才会触发。
+//            'heartbeat_check_interval' => $config['reactor_num'] ?? 5,//心跳检测 遍历所有连接，如果该连接在60秒内，没有向服务器发送任何数据，此连接将被强制关闭。
+//            'heartbeat_idle_time' => $config['reactor_num'] ?? 10,//一个TCP连接如果在10秒内未向服务器端发送数据，将会被切断。
+        );
+//        if(isset($config['task_worker_num']) && $config['task_worker_num']){
+        $swoole_config['dispatch_mode'] =  $config['dispatch_mode'] ?? 2;
+        $swoole_config['task_worker_num'] =  $config['task_worker_num'] ?? 8;  //task进程的数量
+//        }
+
+        if(isset($config['tcp_defer_accept']) && $config['tcp_defer_accept']){
+            $swoole_config['tcp_defer_accept'] =  $config['tcp_defer_accept']; //此参数设定一个秒数，当客户端连接连接到服务器时，在约定秒数内并不会触发accept，直到有数据发送，或者超时时才会触发。
+        }
+
+        if(isset($config['heartbeat_check_interval']) && $config['heartbeat_check_interval']){
+            $swoole_config['heartbeat_check_interval'] =  $config['heartbeat_check_interval']; //心跳检测 遍历所有连接，如果该连接在60秒内，没有向服务器发送任何数据，此连接将被强制关闭。
+        }
+
+        if(isset($config['heartbeat_idle_time']) && $config['heartbeat_idle_time']){
+            $swoole_config['heartbeat_idle_time'] =  $config['heartbeat_idle_time']; //一个TCP连接如果在10秒内未向服务器端发送数据，将会被切断。
+        }
+
+        $this->config = $swoole_config;
     }
 
     public function run(){
         global $argv;
         if (empty($argv[1]) or isset($opt['help']))
         {
-            echo "php {$argv[0]} start|stop|reload" ;
+            echo "php {$argv[0]}  {start|stop|reload}\n\r" ;
+            echo "php {$argv[0]}  start -d 守护进程\n\r";
             exit;
         }else if($argv[1] == 'reload'){
             $server_pid = file_get_contents($this->pid);
@@ -27,41 +68,50 @@ class swoole_new_server {
         }else if($argv[1] == 'stop'){
             $this->stop();
         }else if($argv[1] == 'start'){
+            if(isset($argv[2]) && $argv[2] == '-d'){
+               $this->config['daemonize'] = 1;
+            }
             $this->start();
         }else{
             echo "php {$argv[0]} start|stop|reload";
         }
     }
+
     /**
      * 开启 worker
      */
     public function start(){
-        $this->serv = new swoole_server($this->ip, $this->iport, SWOOLE_PROCESS, SWOOLE_SOCK_TCP);
-        $this->serv->set(array(
-            'reactor_num' => 2,//通过此参数来调节poll线程的数量，以充分利用多核
-            'worker_num' => 4,// 开启进程
-            'daemonize' => 1,//是否开启守护进程 0否 1 是
-            'max_request' => 10000,//最大请求处理1万个 //此参数表示worker进程在处理完n次请求后结束运行。manager会重新创建一个worker进程。此选项用来防止worker进程内存溢出。
-            'dispatch_mode' => 2,
-            'task_worker_num' => 8,  //task进程的数量
-           // "task_ipc_mode " => 3 ,  //使用消息队列通信，并设置为争抢模式
-            'backlog' => 128,//Listen队列长度
-            'pid_file'=>__DIR__.'/server.pid',//进程唯一标识ID
-            'log_level' => 1,
-            'tcp_defer_accept' => 5,//此参数设定一个秒数，当客户端连接连接到服务器时，在约定秒数内并不会触发accept，直到有数据发送，或者超时时才会触发。
-            'heartbeat_check_interval' => 5,//心跳检测 遍历所有连接，如果该连接在60秒内，没有向服务器发送任何数据，此连接将被强制关闭。
-            'heartbeat_idle_time' => 10,//一个TCP连接如果在10秒内未向服务器端发送数据，将会被切断。
-        ));
+        $this->serv = new server($this->ip, $this->iport, SWOOLE_PROCESS, SWOOLE_SOCK_TCP);
+
+        $this->serv->set($this->config);
         $this->pid = __DIR__.'/server.pid';
-//        swoole_set_process_name('test'); //这个设置进程数的 名称 不支持MAC
+        $flag = 0;
+        $os_name=PHP_OS;
+        if(strpos($os_name,"Linux")!==false){
+            $flag = 1;
+        }
+        if($flag > 0){
+            swoole_set_process_name($this->process_name); //这个设置进程数的 名称 不支持MAC
+        }
         $this->serv->on('Receive', array($this, 'onReceive'));
         $this->serv->on('Task', array($this, 'onTask'));
         $this->serv->on('Finish', array($this, 'onFinish'));
         $this->serv->on('Close',array($this,'onClose'));
+        $this->serv->on('WorkerStart', array($this,'workStart'));
         $this->serv->start();
 
     }
 
+    /**
+     * workstart
+     */
+    public function workStart($serv,$worker_id){
+        if($worker_id >= $serv->setting['worker_num']) {
+            swoole_set_process_name("$this->process_name tasker");
+        } else {
+            swoole_set_process_name("$this->process_name worker");
+        }
+    }
     /**
      * 关闭 worker 进程
      */
@@ -71,6 +121,7 @@ class swoole_new_server {
             exit('server进程不存在');
         }
         posix_kill($server_pid, SIGTERM);
+
     }
 
     public function setPid($dir){
@@ -85,7 +136,6 @@ class swoole_new_server {
      */
     public function onReceive($serv, $fd, $from_id, $data){
         $data = json_decode($data,true);
-        print_r($data);
         $fun = [$data['class'],$data['fun']];
         $param = $data['data'];
         $type = $data['type'];
@@ -104,7 +154,6 @@ class swoole_new_server {
             }else{
                 echo '已断开';
             }
-
         }
         //$serv->close($fd);     //关闭客户端连接
     }
@@ -164,26 +213,10 @@ class swoole_new_server {
     }
 
 }
-/**
- * Yii console bootstrap file.
- */
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/vendor/yiisoft/yii2/Yii.php';
-require __DIR__ . '/common/config/bootstrap.php';
-require __DIR__ . '/console/config/bootstrap.php';
-
-$config = yii\helpers\ArrayHelper::merge(
-    require __DIR__ . '/common/config/main.php',
-    require __DIR__ . '/common/config/main-local.php',
-    require __DIR__ . '/swoole/config/main.php',
-    require __DIR__ . '/swoole/config/main-local.php'
-);
 $pid = __DIR__.'/server.pid';//寻找进程id
-$swoole = new swoole_new_server();
+
+$swoole = new \swoole\swoole_new_server();
 
 $data = [
     'class'=>'swoole\controllers\test',
